8e9f52bbb6d5b4827a2b700d45a340268b1cd8e9
mahfudzalfa <mahfudzworksss@gmail.com>
2025-09-14 22:01:35 +0700

    pesan commit tes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/Project_MyPortofolio/", // 👈 ini penting untuk GitHub Pages
   plugins: [react(), tailwindcss()],
-  base: "/Project_MyPortofolio/",
 });
